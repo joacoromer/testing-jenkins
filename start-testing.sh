9ae02ec08ceb91465ea9e901eb332e694278a51b
Joaquin Romero <54591874+joacoromer@users.noreply.github.com>
2022-12-28 14:09:04 -0300

    Update start-testing.sh

--- start-testing.sh
+++ start-testing.sh
@@ -5,8 +5,15 @@ echo "Iniciaremos el testing!"
 docker compose build
 docker compose up 
 
-resp=$(docker compose ps | grep testing | awk '{print $5}')
+resp=$(docker compose ps | grep testing)
+
+if [[ "$resp" =~ "Exit 0" ]];
+then
+    codeError=0
+else
+    codeError=1
+fi
 
 echo "Done!"
 
-exit $resp 
+exit $codeError 
